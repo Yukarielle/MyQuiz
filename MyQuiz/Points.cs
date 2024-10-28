928be9511f4e9abda9005bf9983365605262f16e
Angelina Kühn <Angelina.Kuehn@training.lug-ag.de>
2024-10-28 17:36:57 +0100

    first commit

---
+++ MyQuiz/Points.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace MyQuiz
+{
+	public class Points : INotifyPropertyChanged
+	{
+		private int _pts;
+
+		public int Pts
+		{
+			get => _pts;
+			set
+			{
+			if(_pts != value)
+			{
+				_pts = value;
+					OnPropertyChanged();
+			}
+
+			}
+
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+
+	}
+}
